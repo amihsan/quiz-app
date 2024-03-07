22629268be7d1f40d083cc7447a41a99f5e317c5
amihsan <rumi.apece.du@gmail.com>
2024-03-07 21:41:10 +0100

    test

--- docker/init-letsencrypt.sh
+++ docker/init-letsencrypt.sh
@@ -36,23 +36,23 @@ data_path="./docker/nginx/certbot"
 email="$EMAIL" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
+if [ -d "$data_path" ]; then
+  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+    exit
+  fi
+fi
+
+
 # if [ -d "$data_path" ]; then
-#   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-#   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-#     exit
-#   fi
+#   decision="n" # Auto choose "No" if directory exists
+# else
+#   decision="y" # Auto choose "Yes" if directory doesn't exist
 # fi
 
-
-if [ -d "$data_path" ]; then
-  decision="n" # Auto choose "No" if directory exists
-else
-  decision="y" # Auto choose "Yes" if directory doesn't exist
-fi
-
-if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-  exit
-fi
+# if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+#   exit
+# fi
 
 
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
