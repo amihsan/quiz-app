1b0d3b0ff7a06a50bdcdcea79fea7ebad15972b4
amihsan <rumi.apece.du@gmail.com>
2024-03-07 19:37:55 +0100

    cicd

--- docker/init-letsencrypt.sh
+++ docker/init-letsencrypt.sh
@@ -34,29 +34,25 @@ domains=($DOMAIN www.$DOMAIN)
 rsa_key_size=4096
 data_path="./docker/nginx/certbot"
 email="$EMAIL" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
-
-if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-    exit
-  fi
-fi
-
-
-###################
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 # if [ -d "$data_path" ]; then
-#   decision="n" # Auto choose "No" if directory exists
-# else
-#   decision="y" # Auto choose "Yes" if directory doesn't exist
+#   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+#   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+#     exit
+#   fi
 # fi
 
-# if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-#   exit
-# fi
 
-#############################
+if [ -d "$data_path" ]; then
+  decision="n" # Auto choose "No" if directory exists
+else
+  decision="y" # Auto choose "Yes" if directory doesn't exist
+fi
+
+if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+  exit
+fi
 
 
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
