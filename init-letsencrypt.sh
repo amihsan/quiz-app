4213ba51af31fa7c2b07e3a49ebded33bd728143
amihsan <rumi.apece.du@gmail.com>
2024-03-08 15:17:31 +0100

    test

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -47,7 +47,7 @@ domains=(lebentest.online www.lebentest.online)
 rsa_key_size=4096
 data_path="./docker/nginx/certbot"
 email="" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -105,7 +105,7 @@ esac
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
 docker-compose run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www/certbot \
+  certbot certonly --webroot -w /usr/share/nginx/html \
     $staging_arg \
     $email_arg \
     $domain_args \
