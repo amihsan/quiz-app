c386143dcd32e43d77a811916c24c9c70f44bf57
amihsan <rumi.apece.du@gmail.com>
2024-08-22 17:08:59 +0200

    login and register background updated

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -7,6 +7,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import PasswordResetEmail from "./PasswordResetEmail";
+import backgroundImage from "../assets/background.jpg"; // Import your background image
 
 const Login = () => {
   const { dispatch } = useAuth();
@@ -106,110 +107,122 @@ const Login = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="p-8 bg-white rounded-md shadow-lg w-96">
-        <h2 className="text-3xl font-bold mb-4 text-center text-indigo-600">
-          Login
-        </h2>
-        <form onSubmit={handleLogin} className="space-y-4">
-          <div>
-            <label
-              htmlFor="username_or_email"
-              className="block text-sm font-medium text-gray-600"
-            >
-              Username or Email:
-            </label>
-            <input
-              type="text"
-              id="username_or_email"
-              name="username_or_email"
-              value={formData.username_or_email}
-              onChange={handleInputChange}
-              className={`mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-indigo-600 ${
-                usernameError ? "border-red-500" : ""
-              }`}
-            />
-            {usernameError && (
-              <p className="text-red-500 mt-1">{usernameError}</p>
-            )}
-          </div>
-          <div className="relative">
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-600"
-            >
-              Password:
-            </label>
-            <input
-              type={showPassword ? "text" : "password"}
-              id="password"
-              name="password"
-              value={formData.password}
-              onChange={handleInputChange}
-              className={`mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-indigo-600 ${
-                passwordError ? "border-red-500" : ""
-              }`}
-            />
-            <span
-              className="absolute top-8 right-3 text-indigo-600 cursor-pointer"
-              onClick={togglePasswordVisibility}
-            >
-              <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
-            </span>
-            {passwordError && (
-              <p className="text-red-500 mt-1">{passwordError}</p>
-            )}
-          </div>
-          <div className="flex items-center">
-            <input
-              type="checkbox"
-              id="rememberMe"
-              name="rememberMe"
-              checked={formData.rememberMe}
-              onChange={handleCheckboxChange}
-              className="mr-2"
-            />
-            <label htmlFor="rememberMe" className="text-sm text-gray-600">
-              Remember Me
-            </label>
-          </div>
-          {error && <p className="text-red-500 mt-2">{error}</p>}
-          <div>
-            <button
-              type="submit"
-              className="w-full bg-indigo-600 text-white p-2 rounded-md hover:bg-indigo-700 focus:outline-none"
-              disabled={loading}
-            >
-              {loading ? (
-                <>
-                  <FontAwesomeIcon icon={faSpinner} spin className="mr-2" />
-                  Logging in...
-                </>
-              ) : (
-                "Login"
+    <div
+      className="min-h-screen flex items-center justify-center bg-gray-100"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="flex items-center justify-center bg-gray-100">
+        <div className="p-8 bg-white rounded-md shadow-lg w-96">
+          <h2 className="text-3xl font-bold mb-4 text-center text-indigo-600">
+            Login
+          </h2>
+          <form onSubmit={handleLogin} className="space-y-4">
+            <div>
+              <label
+                htmlFor="username_or_email"
+                className="block text-sm font-medium text-gray-600"
+              >
+                Username or Email:
+              </label>
+              <input
+                type="text"
+                id="username_or_email"
+                name="username_or_email"
+                value={formData.username_or_email}
+                onChange={handleInputChange}
+                className={`mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-indigo-600 ${
+                  usernameError ? "border-red-500" : ""
+                }`}
+              />
+              {usernameError && (
+                <p className="text-red-500 mt-1">{usernameError}</p>
               )}
-            </button>
+            </div>
+            <div className="relative">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-600"
+              >
+                Password:
+              </label>
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleInputChange}
+                className={`mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-indigo-600 ${
+                  passwordError ? "border-red-500" : ""
+                }`}
+              />
+              <span
+                className="absolute top-8 right-3 text-indigo-600 cursor-pointer"
+                onClick={togglePasswordVisibility}
+              >
+                <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
+              </span>
+              {passwordError && (
+                <p className="text-red-500 mt-1">{passwordError}</p>
+              )}
+            </div>
+            <div className="flex items-center">
+              <input
+                type="checkbox"
+                id="rememberMe"
+                name="rememberMe"
+                checked={formData.rememberMe}
+                onChange={handleCheckboxChange}
+                className="mr-2"
+              />
+              <label htmlFor="rememberMe" className="text-sm text-gray-600">
+                Remember Me
+              </label>
+            </div>
+            {error && <p className="text-red-500 mt-2">{error}</p>}
+            <div>
+              <button
+                type="submit"
+                className="w-full bg-indigo-600 text-white p-2 rounded-md hover:bg-indigo-700 focus:outline-none"
+                disabled={loading}
+              >
+                {loading ? (
+                  <>
+                    <FontAwesomeIcon icon={faSpinner} spin className="mr-2" />
+                    Logging in...
+                  </>
+                ) : (
+                  "Login"
+                )}
+              </button>
+            </div>
+          </form>
+          <div className="mt-4 text-gray-600 text-center">
+            <span
+              className="text-indigo-600 cursor-pointer hover:underline"
+              onClick={handleResetModalToggle}
+            >
+              Forgot Password?
+            </span>
           </div>
-        </form>
-        <div className="mt-4 text-gray-600 text-center">
-          <span
-            className="text-indigo-600 cursor-pointer hover:underline"
-            onClick={handleResetModalToggle}
-          >
-            Forgot Password?
-          </span>
+          <p className="mt-4 text-gray-600 text-center">
+            Don't have an account?{" "}
+            <Link
+              to="/register"
+              className="text-indigo-600 hover:underline ml-1"
+            >
+              Register here
+            </Link>
+            .
+          </p>
         </div>
-        <p className="mt-4 text-gray-600 text-center">
-          Don't have an account?{" "}
-          <Link to="/register" className="text-indigo-600 hover:underline ml-1">
-            Register here
-          </Link>
-          .
-        </p>
+        {showResetModal && (
+          <PasswordResetEmail onCancel={handleResetModalClose} />
+        )}
       </div>
-      {showResetModal && (
-        <PasswordResetEmail onCancel={handleResetModalClose} />
-      )}
     </div>
   );
 };
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -5,6 +5,7 @@ import { registerUser } from "../api";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import backgroundImage from "../assets/background.jpg"; // Import your background image
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -72,18 +73,18 @@ const Register = () => {
 
   const handleRegister = async (e) => {
     e.preventDefault();
-  
+
     if (!validateForm()) {
       return;
     }
-  
+
     setLoading(true);
-  
+
     try {
       // Call the registerUser function from the API
       await registerUser(formData);
       setError("");
-  
+
       // Use the navigate function to go to the /login route after successful registration
       navigate("/login");
     } catch (error) {
@@ -97,7 +98,6 @@ const Register = () => {
       setLoading(false);
     }
   };
-  
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
@@ -108,133 +108,144 @@ const Register = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="p-8 bg-white rounded-md shadow-lg w-96">
-        <h2 className="text-3xl font-bold mb-4 text-center text-green-500">
-          Register
-        </h2>
-        <form onSubmit={handleRegister} className="space-y-4">
-          <div>
-            <label
-              htmlFor="username"
-              className="block text-sm font-medium text-gray-600"
-            >
-              Username:
-            </label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              value={formData.username}
-              onChange={handleInputChange}
-              className={`mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-green-500 ${
-                usernameError ? "border-red-500" : ""
-              }`}
-            />
-            {usernameError && (
-              <p className="text-red-500 mt-1">{usernameError}</p>
-            )}
-          </div>
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-600"
-            >
-              Email:
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleInputChange}
-              className={`mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-green-500 ${
-                emailError ? "border-red-500" : ""
-              }`}
-            />
-            {emailError && <p className="text-red-500 mt-1">{emailError}</p>}
-          </div>
-          <div className="relative">
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-600"
-            >
-              Password:
-            </label>
-            <input
-              type={showPassword ? "text" : "password"}
-              id="password"
-              name="password"
-              value={formData.password}
-              onChange={handleInputChange}
-              className={`mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-green-500 ${
-                passwordError ? "border-red-500" : ""
-              }`}
-            />
-            <span
-              className="absolute top-8 right-3 text-green-500 cursor-pointer"
-              onClick={togglePasswordVisibility}
-            >
-              <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
-            </span>
-            {passwordError && (
-              <p className="text-red-500 mt-1">{passwordError}</p>
-            )}
-          </div>
-          <div className="relative">
-            <label
-              htmlFor="retypePassword"
-              className="block text-sm font-medium text-gray-600"
-            >
-              Retype Password:
-            </label>
-            <input
-              type={showRetypePassword ? "text" : "password"}
-              id="retypePassword"
-              name="retypePassword"
-              value={formData.retypePassword}
-              onChange={handleInputChange}
-              className={`mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-green-500 ${
-                retypePasswordError ? "border-red-500" : ""
-              }`}
-            />
-            <span
-              className="absolute top-8 right-3 text-green-500 cursor-pointer"
-              onClick={toggleRetypePasswordVisibility}
-            >
-              <FontAwesomeIcon icon={showRetypePassword ? faEye : faEyeSlash} />
-            </span>
-            {retypePasswordError && (
-              <p className="text-red-500 mt-1">{retypePasswordError}</p>
-            )}
-          </div>
-          {error && <p className="text-red-500 mt-2">{error}</p>}
-          <div>
-            <button
-              type="submit"
-              className={`w-full bg-green-500 text-white p-2 rounded-md hover:bg-green-600 focus:outline-none ${
-                loading ? "opacity-50 cursor-not-allowed" : ""
-              }`}
-            >
-              {/* {loading ? "Registering..." : "Register"} */}
-              {loading ? (
-                <>
-                  <FontAwesomeIcon icon={faSpinner} spin className="mr-2" />
-                  Registering...
-                </>
-              ) : (
-                "Register"
+    <div
+      className="min-h-screen flex items-center justify-center bg-gray-100"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="flex items-center justify-center bg-gray-100">
+        <div className="p-8 bg-white rounded-md shadow-lg w-96">
+          <h2 className="text-3xl font-bold mb-4 text-center text-green-500">
+            Register
+          </h2>
+          <form onSubmit={handleRegister} className="space-y-4">
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium text-gray-600"
+              >
+                Username:
+              </label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                value={formData.username}
+                onChange={handleInputChange}
+                className={`mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-green-500 ${
+                  usernameError ? "border-red-500" : ""
+                }`}
+              />
+              {usernameError && (
+                <p className="text-red-500 mt-1">{usernameError}</p>
               )}
-            </button>
-          </div>
-        </form>
-        <p className="mt-4 text-gray-600 text-center">
-          Already have an account?
-          <Link to="/login" className="text-green-500 hover:underline ml-1">
-            Login here
-          </Link>
-          .
-        </p>
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-600"
+              >
+                Email:
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                className={`mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-green-500 ${
+                  emailError ? "border-red-500" : ""
+                }`}
+              />
+              {emailError && <p className="text-red-500 mt-1">{emailError}</p>}
+            </div>
+            <div className="relative">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-600"
+              >
+                Password:
+              </label>
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleInputChange}
+                className={`mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-green-500 ${
+                  passwordError ? "border-red-500" : ""
+                }`}
+              />
+              <span
+                className="absolute top-8 right-3 text-green-500 cursor-pointer"
+                onClick={togglePasswordVisibility}
+              >
+                <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
+              </span>
+              {passwordError && (
+                <p className="text-red-500 mt-1">{passwordError}</p>
+              )}
+            </div>
+            <div className="relative">
+              <label
+                htmlFor="retypePassword"
+                className="block text-sm font-medium text-gray-600"
+              >
+                Retype Password:
+              </label>
+              <input
+                type={showRetypePassword ? "text" : "password"}
+                id="retypePassword"
+                name="retypePassword"
+                value={formData.retypePassword}
+                onChange={handleInputChange}
+                className={`mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-green-500 ${
+                  retypePasswordError ? "border-red-500" : ""
+                }`}
+              />
+              <span
+                className="absolute top-8 right-3 text-green-500 cursor-pointer"
+                onClick={toggleRetypePasswordVisibility}
+              >
+                <FontAwesomeIcon
+                  icon={showRetypePassword ? faEye : faEyeSlash}
+                />
+              </span>
+              {retypePasswordError && (
+                <p className="text-red-500 mt-1">{retypePasswordError}</p>
+              )}
+            </div>
+            {error && <p className="text-red-500 mt-2">{error}</p>}
+            <div>
+              <button
+                type="submit"
+                className={`w-full bg-green-500 text-white p-2 rounded-md hover:bg-green-600 focus:outline-none ${
+                  loading ? "opacity-50 cursor-not-allowed" : ""
+                }`}
+              >
+                {/* {loading ? "Registering..." : "Register"} */}
+                {loading ? (
+                  <>
+                    <FontAwesomeIcon icon={faSpinner} spin className="mr-2" />
+                    Registering...
+                  </>
+                ) : (
+                  "Register"
+                )}
+              </button>
+            </div>
+          </form>
+          <p className="mt-4 text-gray-600 text-center">
+            Already have an account?
+            <Link to="/login" className="text-green-500 hover:underline ml-1">
+              Login here
+            </Link>
+            .
+          </p>
+        </div>
       </div>
     </div>
   );
