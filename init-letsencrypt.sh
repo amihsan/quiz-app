ad5673aac52c277b33d101e764244229229c5a43
amihsan <rumi.apece.du@gmail.com>
2024-11-10 21:58:28 +0100

    ssl test

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -26,7 +26,7 @@ domains=($DOMAIN www.$DOMAIN)
 rsa_key_size=4096
 data_path="./certbot"
 email="$EMAIL" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 
 if [ -d "$data_path" ]; then
