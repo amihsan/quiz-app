b02dadb9e1f09091c075467eecd5771efc9480d3
amihsan <rumi.apece.du@gmail.com>
2025-01-11 19:51:00 +0100

    footer year made dynamic

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,10 +2,13 @@
 import React from "react";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear(); // Get the current year dynamically
+
   return (
     <footer className="bg-blue-600 p-4 text-white text-center">
       <p className="text-sm">
-        &copy; 2024 <span className="font-bold">Ihsan@Einbürgerungstest</span>. All rights
+        &copy; {currentYear}{" "}
+        <span className="font-bold">Ihsan@Einbürgerungstest</span>. All rights
         reserved.
       </p>
     </footer>
@@ -13,3 +16,4 @@ const Footer = () => {
 };
 
 export default Footer;
+
