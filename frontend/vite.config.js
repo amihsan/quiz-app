aa751deace7611b9c9bc10cace1329b33682af24
amihsan <rumi.apece.du@gmail.com>
2025-03-18 20:23:26 +0100

    migrated to vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  build: {
-    outDir: "build", // Specifies the directory where the production build will be placed
-  },
   plugins: [react()], // React plugin for JSX support, Fast Refresh, and React optimizations
-  server: {
-    port: 3000, // Optional: Set the port for the development server (default is 3000)
-  },
 });
