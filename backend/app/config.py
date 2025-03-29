1db85e09b1c4e8dfa8ca343bf9810717a920b244
amihsan <rumi.apece.du@gmail.com>
2025-03-29 14:46:31 +0100

    test

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-    MONGODB_URI = os.getenv('MONGODB_URI')
-    DATABASE_NAME = os.getenv('DATABASE_NAME')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'test_secret')  # Default for testing
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'test_jwt_secret')
+    MONGODB_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017/')
+    DATABASE_NAME = os.getenv('DATABASE_NAME', 'test_db')  # Use a test DB
