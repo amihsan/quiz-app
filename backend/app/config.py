02145f42e7f963a77d0c1beab508b7f0e457d8d5
amihsan <rumi.apece.du@gmail.com>
2025-06-18 11:03:54 +0200

    jwt token expire time updated

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,11 +1,13 @@
 # app/config.py
 import os
 from dotenv import load_dotenv
+from datetime import timedelta
 
 load_dotenv()
 
 class Config:
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY') 
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30) # increase duration here
     MONGODB_URI = os.getenv('MONGODB_URI')
     DATABASE_NAME = os.getenv('DATABASE_NAME')
     SECRET_KEY = os.getenv('SECRET_KEY')
